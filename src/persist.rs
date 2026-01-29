9b32c6a602012dba901cc0e135fc2a081932b82d
Sycration <sycration@sycration.xyz>
2026-01-28 18:21:55 -0800

    refactor persist function

--- src/persist.rs
+++ src/persist.rs
@@ -2,24 +2,23 @@ use std::path::Path;
 
 use anyhow::{Ok, Result};
 use serde::{Deserialize, Serialize};
-use tokio::{fs::OpenOptions, io::AsyncWriteExt};
+use tokio::{fs::{self, OpenOptions}, io::AsyncWriteExt};
 
 pub async fn persist<T: Serialize + for<'a> Deserialize<'a>>(
     data: &T,
     config_dir: &Path,
     name: &str,
 ) -> anyhow::Result<()> {
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .append(true)
-        .create(true)
-        .open(config_dir.join(format!("{name}.json")))
-        .await?;
-
-    file.set_len(0).await?;
+    let file_path = config_dir.join(format!("{name}.json"));
+    let temp_path = config_dir.join(format!(".{name}.json.tmp"));
+    
+    let mut temp_file = fs::File::create(&temp_path).await?;
     let token_json = serde_json::to_string_pretty(&data)?;
-    file.write_all(token_json.as_bytes()).await?;
+    temp_file.write_all(token_json.as_bytes()).await?;
+    temp_file.sync_all().await?;
+    
+    fs::rename(&temp_path, &file_path).await?;
+    
     Ok(())
 }
 
